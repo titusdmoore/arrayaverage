ae84fdaeac9d23ed5042c55c64532d5248310f72
Titus Moore <titusdmoore@gmail.com>
2019-07-25 10:59:14 -0400

    Added support for Dynamic Array length

--- AverageArrayProject/Program.cs
+++ AverageArrayProject/Program.cs
@@ -5,19 +5,20 @@ namespace AverageArrayProject {
         static void Main(string[] args) {
 
             // Another way to set arrays
-            int[] scores = { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63 };
+            int[] scores = { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63, 55, 78, 99 };
             int index = 0;
-            int sum = 0;
+            decimal sum = 0m;
             decimal average = 0.0m;
+            int total = scores.GetLength(0);
 
             // Begin a loop that runs through each index in Scores
-            while (index < 12) {
+            while (index < total) {
                 sum = sum + scores[index];
                 index++;
             }
 
             // When while has indexed run the if statement to find average
-            if (index == 12) {
+            if (index == total) {
                 average = sum / 12m;
             }
 
