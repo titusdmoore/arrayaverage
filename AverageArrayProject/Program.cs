37e26c469daaab49c2466c37560849eae6604440
Titus Moore <titusdmoore@gmail.com>
2019-07-25 10:08:29 -0400

    Added Documentation

--- AverageArrayProject/Program.cs
+++ AverageArrayProject/Program.cs
@@ -3,17 +3,25 @@
 namespace AverageArrayProject {
     class Program { 
         static void Main(string[] args) {
+
+            // Another way to set arrays
             int[] scores = { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63 };
             int index = 0;
             int sum = 0;
             decimal average = 0.0m;
+
+            // Begin a loop that runs through each index in Scores
             while (index < 12) {
                 sum = sum + scores[index];
                 index++;
             }
+
+            // When while has indexed run the if statement to find average
             if (index == 12) {
                 average = sum / 12m;
             }
+
+            // Print Average Message
             Console.WriteLine($"The Average is {average}%");
         }
     }
