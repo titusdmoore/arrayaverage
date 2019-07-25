122b0b0dbf03f42b3842fe385f65cef1e18dd64f
Titus Moore <titusdmoore@gmail.com>
2019-07-25 10:05:05 -0400

    Reformatted for readability

--- AverageArrayProject/Program.cs
+++ AverageArrayProject/Program.cs
@@ -6,11 +6,11 @@ namespace AverageArrayProject {
             int[] scores = { 71, 78, 89, 73, 68, 73, 96, 71, 72, 65, 87, 63 };
             int index = 0;
             int sum = 0;
+            decimal average = 0.0m;
             while (index < 12) {
                 sum = sum + scores[index];
                 index++;
             }
-            decimal average = 0.0m;
             if (index == 12) {
                 average = sum / 12m;
             }
